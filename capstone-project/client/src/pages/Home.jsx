f32dc9cc2de14b5eb938f85ea9fbed3e27316d8e
nalex1 <alxnkuru@gmail.com>
2026-02-27 17:45:31 +0200

    adding env file in client and modifie home app

--- capstone-project/client/src/pages/Home.jsx
+++ capstone-project/client/src/pages/Home.jsx
@@ -1,28 +1,48 @@
 import { useState } from "react";
+import axios from "axios";
 import SearchForm from "../components/SearchForm";
 import FlightResults from "../components/FlightResults";
-//import { api } from "../api/amadeus";
 
 export default function Home() {
   const [flights, setFlights] = useState([]);
+  const [loading, setLoading] = useState(false);
 
   const searchFlights = async (form) => {
     try {
-      const res = await api.get("/flights", {
-        params: form,
-      });
-      setFlights(res.data);
+      setLoading(true);
+
+      const res = await axios.get(
+        `${import.meta.env.VITE_API_URL}/api/flights`,
+        {
+          params: {
+            originLocationCode: form.from,
+            destinationLocationCode: form.to,
+            departureDate: form.date,
+            adults: 1,
+          },
+        }
+      );
+
+      setFlights(res.data.data || []);
     } catch (error) {
       console.error(error);
       alert("Failed to fetch flights");
+    } finally {
+      setLoading(false);
     }
   };
 
   return (
     <div className="max-w-7xl mx-auto">
       <SearchForm onSearch={searchFlights} />
+
+      {loading && (
+        <p className="text-center mt-4 text-blue-600 font-semibold">
+          Searching flights...
+        </p>
+      )}
+
       <FlightResults flights={flights} />
-      
     </div>
   );
 }
